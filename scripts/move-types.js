6aa50cb7ccaeda7804aaf76706b5e16c155ee728
thundermiracle <cissoliu@gmail.com>
2019-08-20 22:34:08 +0900

    remove unecessary d.ts when build

--- scripts/move-types.js
+++ scripts/move-types.js
@@ -1,19 +1,12 @@
 import path from 'path';
-import fse from 'fs-extra';
-import glob from 'glob';
-
-function typescriptMV(from, to) {
-  const files = glob.sync('**/*.d.ts', { cwd: from });
-  const cmds = files.map(file =>
-    fse.move(path.resolve(from, file), path.resolve(to, file)),
-  );
-  return Promise.all(cmds);
-}
+import operTypes from './oper-types';
 
 async function run() {
   // TypeScript
   const from = path.resolve(__dirname, '../src');
-  await Promise.all([typescriptMV(from, path.resolve(__dirname, '../dist'))]);
+  await Promise.all([
+    operTypes('move', from, path.resolve(__dirname, '../dist')),
+  ]);
 }
 
 run();
