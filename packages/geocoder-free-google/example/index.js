fb007e31d4da9a5d6d175c5bc38ffdb30f171ab5
thundermiracle <cissoliu@gmail.com>
2019-08-30 21:30:01 +0900

    fix @geocoder-free/google reference error

--- packages/geocoder-free-google/example/index.js
+++ packages/geocoder-free-google/example/index.js
@@ -1,4 +1,7 @@
-const { GetLatLngByAddress, GetLatLngByGooGl } = require('geocoder-free');
+const {
+  GetLatLngByAddress,
+  GetLatLngByGooGl,
+} = require('@geocoder-free/google');
 
 GetLatLngByAddress('tokyo tower').then(console.log);
 GetLatLngByGooGl('https://goo.gl/maps/TmYFq1DdceHVpsHV6').then(console.log);
