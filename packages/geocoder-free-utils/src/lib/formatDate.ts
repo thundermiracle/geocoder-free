d896a01ee7c34312299ff65e40c3d2b97097e6bd
thundermiracle <cissoliu@gmail.com>
2019-09-06 21:03:16 +0900

    fix tslint error

--- packages/geocoder-free-utils/src/lib/formatDate.ts
+++ packages/geocoder-free-utils/src/lib/formatDate.ts
@@ -1,7 +1,7 @@
-function formatDate(
+function formatStrDate(
   date: Date | string | null,
-  format: string = 'yyyy/MM/dd HH:mm:ss',
-) {
+  format = 'yyyy/MM/dd HH:mm:ss',
+): string {
   if (date == null || date === '') {
     return '';
   }
@@ -13,23 +13,31 @@ function formatDate(
     dtDate = date;
   }
 
-  format = format.replace(/yyyy/g, '' + dtDate.getFullYear());
-  format = format.replace(/MM/g, ('0' + (dtDate.getMonth() + 1)).slice(-2));
-  format = format.replace(/dd/g, ('0' + dtDate.getDate()).slice(-2));
-  format = format.replace(/HH/g, ('0' + dtDate.getHours()).slice(-2));
-  format = format.replace(/mm/g, ('0' + dtDate.getMinutes()).slice(-2));
-  format = format.replace(/ss/g, ('0' + dtDate.getSeconds()).slice(-2));
+  let formatStr = format;
 
-  format = format.replace(/yy/g, ('' + dtDate.getFullYear()).slice(-2));
-  format = format.replace(/M/g, ('' + (dtDate.getMonth() + 1)).slice(-2));
-  format = format.replace(/d/g, ('' + dtDate.getDate()).slice(-2));
-  format = format.replace(/H/g, ('' + dtDate.getHours()).slice(-2));
-  format = format.replace(/m/g, ('' + dtDate.getMinutes()).slice(-2));
-  format = format.replace(/s/g, ('' + dtDate.getSeconds()).slice(-2));
+  formatStr = formatStr.replace(/yyyy/g, dtDate.getFullYear().toString());
+  formatStr = formatStr.replace(
+    /MM/g,
+    ('0' + (dtDate.getMonth() + 1)).slice(-2),
+  );
+  formatStr = formatStr.replace(/dd/g, ('0' + dtDate.getDate()).slice(-2));
+  formatStr = formatStr.replace(/HH/g, ('0' + dtDate.getHours()).slice(-2));
+  formatStr = formatStr.replace(/mm/g, ('0' + dtDate.getMinutes()).slice(-2));
+  formatStr = formatStr.replace(/ss/g, ('0' + dtDate.getSeconds()).slice(-2));
 
-  format = format.replace(/SSS/g, ('00' + dtDate.getMilliseconds()).slice(-3));
+  formatStr = formatStr.replace(/yy/g, ('' + dtDate.getFullYear()).slice(-2));
+  formatStr = formatStr.replace(/M/g, ('' + (dtDate.getMonth() + 1)).slice(-2));
+  formatStr = formatStr.replace(/d/g, ('' + dtDate.getDate()).slice(-2));
+  formatStr = formatStr.replace(/H/g, ('' + dtDate.getHours()).slice(-2));
+  formatStr = formatStr.replace(/m/g, ('' + dtDate.getMinutes()).slice(-2));
+  formatStr = formatStr.replace(/s/g, ('' + dtDate.getSeconds()).slice(-2));
 
-  return format;
+  formatStr = formatStr.replace(
+    /SSS/g,
+    ('00' + dtDate.getMilliseconds()).slice(-3),
+  );
+
+  return formatStr;
 }
 
-export default formatDate;
+export default formatStrDate;
